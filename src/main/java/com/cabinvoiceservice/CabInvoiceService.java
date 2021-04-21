cdd80b1e923a6cdb4b97b371fb021513ae847cf1
ganesh-48 <ganeshsrananavare@gmail.com>
2021-04-21 04:58:23 -0700

    ADDED: Calculated total fare for the journey

--- src/main/java/com/cabinvoiceservice/CabInvoiceService.java
+++ src/main/java/com/cabinvoiceservice/CabInvoiceService.java
@@ -1,7 +1,24 @@
 package com.cabinvoiceservice;
 
+/*@Description- Cab Invoice Generator Problem.
+* Welcome to Cab Invoice Services.
+* Calculate the total fare for the journey.
+* And calculating minimum fare also. */
 public class CabInvoiceService {
+    private static final int COST_PER_TIME = 1;
+    private static final double MINIMUM_COST_PER_KILOMETER = 10;
+    private static final double MINIMUM_FARE = 5;
+
     public static void main(String[] args) {
         System.out.println("Welcome to Cab Invoice Service");
+    }
+
+    /*@Description- Calculate the total fare for the journey.
+     * And calculating minimum fare also.*/
+    public double calculateFare(double distance, int time) {
+        double totalFare = distance * MINIMUM_COST_PER_KILOMETER + time * COST_PER_TIME;
+        if (totalFare < MINIMUM_FARE)
+            return MINIMUM_FARE;
+        return totalFare;
     }
 }
--- src/test/java/com/cabinvoiceservice/CabInvoiceServiceTest.java
+++ src/test/java/com/cabinvoiceservice/CabInvoiceServiceTest.java
@@ -1,4 +1,24 @@
 package com.cabinvoiceservice;
 
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.Test;
+
 public class CabInvoiceServiceTest {
+    @Test
+    public void givenDistanceAndTimeShouldReturnTotalFare() {
+        CabInvoiceService cabInvoiceService = new CabInvoiceService();
+        double distance = 2.0;
+        int time = 5;
+        double fare = cabInvoiceService.calculateFare(distance, time);
+        Assertions.assertEquals(25, fare, 0.0);
+    }
+
+    @Test
+    public void givenLessDistanceAndTimeShouldReturnMinimumFare() {
+        CabInvoiceService cabInvoiceService = new CabInvoiceService();
+        double distance = 0.2;
+        int time = 1;
+        double fare = cabInvoiceService.calculateFare(distance,time);
+        Assertions.assertEquals(5, fare, 0.0);
+    }
 }
