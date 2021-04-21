ffe6ae653be6d55136c7ba8882889bfbe4c5cbe7
ganesh-48 <ganeshsrananavare@gmail.com>
2021-04-21 03:53:07 -0700

    ADDED: Wecome to Cab Invoice Service

---
+++ src/main/java/com/cabinvoiceservice/CabInvoiceService.java
@@ -0,0 +1,7 @@
+package com.cabinvoiceservice;
+
+public class CabInvoiceService {
+    public static void main(String[] args) {
+        System.out.println("Welcome to Cab Invoice Service");
+    }
+}
---
+++ src/test/java/com/cabinvoiceservice/CabInvoiceServiceTest.java
@@ -0,0 +1,4 @@
+package com.cabinvoiceservice;
+
+public class CabInvoiceServiceTest {
+}
